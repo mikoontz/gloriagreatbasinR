ae3aa462a1ea0255a3a58ab0b309603fc7577b3c
Michael Koontz <mikoontz@gmail.com>
2021-06-17 15:30:56 -0600

    sources to make this work.

--- generate-photo-point-qr-codes.R
+++ generate-photo-point-qr-codes.R
@@ -1,1 +1,6 @@
+https://github.com/hrbrmstr/qrencoder to generate the codes
 devtools::install_github("hrbrmstr/qrencoder")
+
+and https://github.com/brianwdavis/quadrangle to read them
+
+and https://cran.r-project.org/web/packages/exifr/index.html to get the time stamp from the photo’s metadata
