ab7e51b40da5a33a50ca377a73e7dc8f5e6a0d97
Michael Koontz <mikoontz@gmail.com>
2021-06-18 14:44:57 -0600

    extract info from QR codes in images and create new filenames for the photos (including metadata from images)

--- extract-info-from-qr-code.R
+++ extract-info-from-qr-code.R
@@ -2,8 +2,24 @@ library(exifr)
 # devtools::install_github("brianwdavis/quadrangle", INSTALL_opts = "--no-multiarch")
 # You may not need `INSTALL_opts = "--no-multiarch"` depending on your system.
 library(quadrangle)
+library(stringr)
+library(lubridate)
+library(pbapply)
 
 photo_files <- list.files("data/raw/", full.names = TRUE)
-metadata <- exifr::read_exif(path = photo_files)
 
-qr_codes_from_photos <- quadrangle::qr_scan(photo_files)
+qr_extract <- function(file) {
+  qr <- quadrangle::qr_scan(file)
+  metadata <- exifr::read_exif(path = file)
+  photo_date <- lubridate::ymd_hms(metadata$FileModifyDate)
+  month_out <- str_pad(string = month(photo_date), width = 2, side = "left", pad = "0")
+  day_out <- str_pad(string = day(photo_date), width = 2, side = "left", pad = "0")
+  out <- paste0(paste(qr$values$value, collapse = ""), "_", year(photo_date), month_out, day_out, "_", basename(file))
+  
+  return(out)
+}
+
+new_photo_filenames <- 
+  pbsapply(photo_files, FUN = qr_extract)
+
+new_photo_filenames
